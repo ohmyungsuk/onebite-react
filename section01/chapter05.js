12fafa9bfb60c2c0ab1d7e7489c268de993c2549
오명석 <>
2026-02-21 23:57:22 +0900

    자료형 실습

---
+++ section01/chapter05.js
@@ -0,0 +1,27 @@
+//1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. string Type
+let myName = "오명석";
+let myLocation = "서울";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 살고 있습니다.`;
+//템플릿 리터럴 문법 (중요)
+// 3. boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. null Type
+let empty = null;
+
+// 5. undefined Type
+let none;
+console.log(none);
