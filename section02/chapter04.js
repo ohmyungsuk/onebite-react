c21e65f36a4fc5d2b83ebe1251c547902a5330d1
ohmyungsuk <qnrtjdnfz09@naver.com>
2026-03-13 16:11:34 +0900

    spread 연산자와 Rest 매개변수

---
+++ section02/chapter04.js
@@ -0,0 +1,27 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, ...arr1, 5, 6];
+
+let obj1 = {
+  a: 1,
+  b: 2,
+};
+let obj2 = {
+  ...obj1,
+  c: 3,
+  d: 4,
+}
+
+function funcA(p1, p2, p3) {
+  console.log(p1, p2, p3);
+
+}
+
+funcA(...arr1);
+
+fuction funcB(one, two, ...rest)  {
+  console.log(rest);
+
+}
+
+  funcB(...arr1);
+
