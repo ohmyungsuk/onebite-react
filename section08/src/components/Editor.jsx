a26ad831342079570743c9e43a95cb9f54c7e9eb
ohmyungsuk <qnrtjdnfz09@naver.com>
2026-03-18 16:48:57 +0900

    Create 투두 추가하기

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,11 +1,41 @@
+import { useRef, useState } from "react";
 import "./Editor.css";
 
-const Editor = () => {
-    return <div className="Editor">
-        <input placeholder="새로운 Todo..."/>
-        <button>추가</button>
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const inputRef = useRef();
 
-    </div>;
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={inputRef}
+        value={content}
+        onChange={onChangeContent}
+        onKeyDown={onKeydown}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
 };
 
 export default Editor;
